8ffbbe248a3b82fe6dab15a4a255f12bd08be1ed
kerm11 <1837278602@qq.com>
2020-10-22 20:18:56 +0800

    1022

---
+++ A83-ZM-S2/src/com/yc/damai/dao/User.java
@@ -0,0 +1,17 @@
+package com.yc.damai.dao;
+
+import java.util.List;
+
+import com.sun.javafx.collections.MappingChange.Map;
+import com.yc.damai.util.DBHelper;
+
+public class User {
+	DBHelper dbHelper= new DBHelper();
+	
+	
+//	public List<Map<String, Object>> insert(){
+//		String sql="select * from dm_user";
+//		return	dbHelper.query(sql);
+//	}
+
+}
